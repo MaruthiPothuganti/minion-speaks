1f2f6883034808f0c68cf15674dd2d013f435c82
Pothuganti Maruthi <maruthicharypothuganti@gmail.com>
2021-01-02 23:37:36 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = 'https://api.funtranslations.com/translate/minion.json'
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text" + input
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
 
 function errorHandler(error){
     console.log("error occurred", error);
     alert("something wrong with the server! Please tryt after sometime")
 }
-function clickHandler(){
+function clickHandler() {
      var inputText = txtInput.value;
 
 
